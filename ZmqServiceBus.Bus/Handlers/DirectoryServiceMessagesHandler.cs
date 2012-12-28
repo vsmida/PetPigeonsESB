00bdf7df24743b2d239b462612f57766e8cd8366
JJ@Corei3 <JJ@Corei3>
2012-12-28 08:12:17 +0100

    update directory service handlers

--- ZmqServiceBus.Bus/Handlers/DirectoryServiceMessagesHandler.cs
+++ ZmqServiceBus.Bus/Handlers/DirectoryServiceMessagesHandler.cs
@@ -1,17 +1,24 @@
 ﻿using ZmqServiceBus.Bus.InfrastructureMessages;
+using ZmqServiceBus.Bus.Transport;
 using ZmqServiceBus.Bus.Transport.Network;
+using System.Linq;
 
 namespace ZmqServiceBus.Bus.Handlers
 {
-    public class DirectoryServiceMessagesHandler : IEventHandler<PeerConnected>, ICommandHandler<InitializeTopologyAndMessageSettings>
+    public class DirectoryServiceMessagesHandler : IEventHandler<PeerConnected>, ICommandHandler<InitializeTopologyAndMessageSettings>,
+                                                   ICommandHandler<RegisterPeerCommand>
     {
         private readonly IPeerManager _peerManager;
+        private readonly IBus _bus;
+        private readonly IReplier _replier;
         private readonly IMessageOptionsRepository _optionsRepository;
 
-        public DirectoryServiceMessagesHandler(IPeerManager peerManager, IMessageOptionsRepository optionsRepository)
+        public DirectoryServiceMessagesHandler(IPeerManager peerManager, IMessageOptionsRepository optionsRepository, IReplier replier, IBus bus)
         {
             _peerManager = peerManager;
             _optionsRepository = optionsRepository;
+            _replier = replier;
+            _bus = bus;
         }
 
         public void Handle(PeerConnected message)
@@ -29,6 +36,17 @@ namespace ZmqServiceBus.Bus.Handlers
             {
                 _optionsRepository.RegisterOptions(messageOption);
             }
+        }
+
+        public void Handle(RegisterPeerCommand item)
+        {
+            _peerManager.RegisterPeer(item.Peer);
+
+            var initCommand = new InitializeTopologyAndMessageSettings(_peerManager.GetAllPeers().ToList(),
+                                                                       _optionsRepository.GetAllOptions());
+            _replier.Reply(initCommand);
+
+            _bus.Publish(new PeerConnected(item.Peer));
         }
     }
 }
